8b302c70a9ee5adcbd212c8bbf76b45377fde324
Fajri135 <corex135@gmail.com>
2024-05-28 11:37:16 +0700

    Membuat class Anstraksiklas dan memberikan akses private dan public

--- Abstraksi_0189/Abstraksi_0189.cpp
+++ Abstraksi_0189/Abstraksi_0189.cpp
@@ -1,6 +1,24 @@
 #include <iostream> 
 using namespace std;
 
+class Abstraksiklas {
+private: string x, y;
+
+public:
+
+    //method untuk mengisi nilai 
+    //priavte member 
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
 int main()
 {
     ;
