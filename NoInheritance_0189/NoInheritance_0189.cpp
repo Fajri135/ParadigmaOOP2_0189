f7b1e4a55e61054dc97f5529f365fac91d487037
Fajri135 <corex135@gmail.com>
2024-05-28 11:02:34 +0700

    Membuat class derivedClass turunan baseClass tapi error karena baseClass tidak dapat digunakan

--- NoInheritance_0189/NoInheritance_0189.cpp
+++ NoInheritance_0189/NoInheritance_0189.cpp
@@ -7,7 +7,12 @@ public:
         cout << "Hallo saya function dari base class";
     }
 };
-
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << "Hallo saya function dari derived class";
+    }
+};
 int main() {
 
 }
--- Overrid_0189/Overrid_0189.cpp
+++ Overrid_0189/Overrid_0189.cpp
@@ -7,8 +7,7 @@ using namespace std;
 class baseClass
 {
 public:
-    virtual void perkenalan()
-    {
+    virtual void perkenalan() final {
         cout << "Hallo saya function dari base class";
     }
 };
