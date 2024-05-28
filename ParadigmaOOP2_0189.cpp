2358949a43a201bb1f24107e7e6726556991cdc5
Fajri135 <corex135@gmail.com>
2024-05-28 10:16:54 +0700

    Menambahkan pure virtual function

--- ParadigmaOOP2_0189.cpp
+++ ParadigmaOOP2_0189.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 class seseorang {
 public:
+    // Pure virtual function
+    virtual void pesan() = 0;
+
+    // Virtual function biasa
+    // Virtual void pesan()
+    // {
+    //   cout << "Pesan dari seseorang" << end;
+    // }
 };
+
 
 int main()
 {
