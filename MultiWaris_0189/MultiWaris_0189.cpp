5eca3e4f288025dc38d0b83db12e4a2c20533e91
Fajri135 <corex135@gmail.com>
2024-05-28 11:45:14 +0700

    Membuat class orang dan memberikan akses public

--- MultiWaris_0189/MultiWaris_0189.cpp
+++ MultiWaris_0189/MultiWaris_0189.cpp
@@ -1,6 +1,18 @@
 #include <iostream> 
 using namespace std;
 
+
+class Orang {
+public:
+	int umur;
+
+	Orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
+
 int main() {
 	;
 }
