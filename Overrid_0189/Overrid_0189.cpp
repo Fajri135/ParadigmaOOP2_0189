8c1a693468f82dd35ca4a3057016cf001d0fc096
Fajri135 <corex135@gmail.com>
2024-05-28 10:34:55 +0700

    Membuat class baseClass

--- Overrid_0189/Overrid_0189.cpp
+++ Overrid_0189/Overrid_0189.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// Class parent
+// Tambahkan final sesudah nama class
+// Untuk mencegah adanya overriding
+class baseClass
+{
+    
+};
+
 int main()
 {
     ;
