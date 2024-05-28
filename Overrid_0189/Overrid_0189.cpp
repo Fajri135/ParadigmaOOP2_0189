da64cc75f18d3f63a7a8c91705fbf741ed1e9904
Fajri135 <corex135@gmail.com>
2024-05-28 10:42:47 +0700

    Membuat class derivedClass turunan dari baseClass dan memberikan akses public

--- Overrid_0189/Overrid_0189.cpp
+++ Overrid_0189/Overrid_0189.cpp
@@ -6,11 +6,24 @@ using namespace std;
 // Untuk mencegah adanya overriding
 class baseClass
 {
-    
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya function dari base class";
+    }
+};
+
+class derivedClass : public baseClass 
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya function dari derived class";
+    }
 };
 
 int main()
 {
-    ;
+   ;
 }
 
